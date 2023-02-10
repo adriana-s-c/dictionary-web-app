4d73f997217b206a994988bcce6407168735616f
Adriana <97173869+a-zajonc@users.noreply.github.com>
2023-02-10 13:01:48 +0100

    Add Heading, Pronunciation and Play Icon

--- src/components/DictionaryApp/WordPage/Heading/index.tsx
+++ src/components/DictionaryApp/WordPage/Heading/index.tsx
@@ -1,6 +1,32 @@
 import * as React from "react";
-import { Box } from "@chakra-ui/react";
+import { Box, Text, Img } from "@chakra-ui/react";
+import playSound from "../../../../images/icon-play.svg";
 
 export function Heading() {
-  return <Box>Heading</Box>;
+  return (
+    <Box
+      display="flex"
+      flexDir="row"
+      w="737px"
+      h="114px"
+      justifyContent="space-between"
+      alignItems="center"
+      marginBottom="50px"
+    >
+      <Box display="flex" flexDir="column">
+        <Text
+          color="2D2D2D"
+          fontSize="64px"
+          fontWeight="700"
+          textShadow="0px 4px 4px #00000040"
+        >
+          keyboard
+        </Text>
+        <Text fontSize="24px" color="#A445ED">
+          /ˈkiːbɔːd/
+        </Text>
+      </Box>
+      <Img src={playSound} alt="play" w="75px" h="75px" />
+    </Box>
+  );
 }
