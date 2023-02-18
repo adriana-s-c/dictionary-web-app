298e73051a581e253828054d9e64595200b919cf
Adriana <97173869+a-zajonc@users.noreply.github.com>
2023-02-18 22:34:45 +0100

    Add Setting Word from Dictionary from Input

--- src/components/DictionaryApp/index.tsx
+++ src/components/DictionaryApp/index.tsx
@@ -3,16 +3,17 @@ import { Box, Text } from "@chakra-ui/react";
 import { NavBar } from "./NavBar";
 import { WordPage } from "./WordPage";
 import { SearchInput } from "./SearchInput";
-import { WordContext } from "../../context";
+import { DictionaryDefitinitionContext } from "../../context";
 
 export function DictionaryApp() {
-  const { word } = React.useContext(WordContext);
-
+  const { dictionaryDefinition } = React.useContext(
+    DictionaryDefitinitionContext
+  );
   return (
     <Box w="737px">
       <NavBar />
       <SearchInput />
-      {word.length < 1 ? <Text>Loading</Text> : <WordPage />}
+      {dictionaryDefinition.length < 1 ? <Text>Loading</Text> : <WordPage />}
     </Box>
   );
 }
--- src/context.ts
+++ src/context.ts
@@ -1,3 +1,5 @@
 import { createContext } from "react";
 
 export const WordContext = createContext<any | undefined>([]);
+
+export const DictionaryDefitinitionContext = createContext<any | undefined>([]);
