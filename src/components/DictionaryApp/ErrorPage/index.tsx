ebdb7c756af49ab7f378fe465d0916000ee51d65
Adriana <97173869+a-zajonc@users.noreply.github.com>
2023-02-20 11:09:41 +0100

    Add Show Error Page

---
+++ src/components/DictionaryApp/ErrorPage/index.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import { Box, Img, Text } from "@chakra-ui/react";
+import { DictionaryDefitinitionContext } from "../../../context";
+import confusedFaceEmoji from "../../../images/confused-face.png";
+
+export function ErrorPage() {
+  const { dictionaryDefinition } = React.useContext(
+    DictionaryDefitinitionContext
+  );
+
+  const errorMessage = dictionaryDefinition.message;
+  const errorResolution = dictionaryDefinition.resolution;
+
+  return (
+    <Box display="flex" flexDir="column" alignItems="center" w="736px">
+      <Img
+        src={confusedFaceEmoji}
+        alt="Confused face emoji"
+        w="64px"
+        marginBottom="24px"
+      />
+      <Text fontSize="20px" fontWeight="700" marginBottom="24px">
+        {dictionaryDefinition.title}
+      </Text>
+      <Text fontSize="18px" fontWeight="400" textAlign="center">
+        {errorMessage.concat(" ", errorResolution)}
+      </Text>
+    </Box>
+  );
+}
--- src/components/DictionaryApp/index.tsx
+++ src/components/DictionaryApp/index.tsx
@@ -4,16 +4,27 @@ import { NavBar } from "./NavBar";
 import { WordPage } from "./WordPage";
 import { SearchInput } from "./SearchInput";
 import { DictionaryDefitinitionContext } from "../../context";
+import { ErrorPage } from "./ErrorPage";
 
 export function DictionaryApp() {
   const { dictionaryDefinition } = React.useContext(
     DictionaryDefitinitionContext
   );
+  function contentRender() {
+    if (dictionaryDefinition.length > 0) {
+      return <WordPage />;
+    }
+    if (dictionaryDefinition.title === "No Definitions Found") {
+      return <ErrorPage />;
+    }
+    <Text>Loading</Text>;
+  }
+
   return (
     <Box w="737px">
       <NavBar />
       <SearchInput />
-      {dictionaryDefinition.length < 1 ? <Text>Loading</Text> : <WordPage />}
+      {contentRender()}
     </Box>
   );
 }
