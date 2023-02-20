0fdd237bde93d1336d59d3f0e3dc42d7c5b413b5
Adriana <97173869+a-zajonc@users.noreply.github.com>
2023-02-20 13:15:08 +0100

    Add Empty Input Error Message

--- src/components/DictionaryApp/SearchInput/index.tsx
+++ src/components/DictionaryApp/SearchInput/index.tsx
@@ -1,45 +1,77 @@
 import * as React from "react";
-import { Img, Input, InputGroup, InputRightElement } from "@chakra-ui/react";
+import {
+  Img,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Text,
+  Box,
+} from "@chakra-ui/react";
 import searchIcon from "../../../images/icon-search.svg";
 import { WordContext } from "../../../context";
 
 export function SearchInput() {
-  const { setWord } = React.useContext(WordContext);
+  const { word, setWord } = React.useContext(WordContext);
   const [inputWord, setInputWord] = React.useState("");
+  const [inputError, setInputError] = React.useState(false);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (inputWord.length > 0) {
+      setInputError(false);
+    }
     return setInputWord(event.target.value);
+  };
+
+  const handleSearchClick = () => {
+    if (inputWord.length === 0) {
+      setInputError(true);
+      setWord(inputWord);
+    }
+    setWord(inputWord);
   };
 
   const handleKeyDown = (event: any) => {
     if (event.key === "Enter") {
+      if (inputWord.length === 0) {
+        setInputError(true);
+      }
       setWord(inputWord);
     }
   };
 
   return (
-    <InputGroup marginBottom="50px">
-      <Input
-        bgColor="#F4F4F4"
-        borderColor="#F4F4F 4"
-        focusBorderColor="#A445ED"
-        fontSize="20px"
-        fontWeight="700"
-        h="64px"
-        borderRadius="16px"
-        w="736px"
-        onChange={handleChange}
-        onKeyDown={handleKeyDown}
-      />
-      <InputRightElement h="64px">
-        <Img
-          src={searchIcon}
-          alt="Search"
-          onClick={() => {
-            setWord(inputWord);
+    <Box marginBottom="52px">
+      <InputGroup>
+        <Input
+          bgColor="#F4F4F4"
+          borderColor="#F4F4F4"
+          focusBorderColor={inputError === true ? "#FF5252" : "#A445ED"}
+          errorBorderColor="#FF5252"
+          fontSize="20px"
+          fontWeight="700"
+          h="64px"
+          borderRadius="16px"
+          w="736px"
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
+          isInvalid={inputError}
+          _placeholder={{
+            color: "#2D2D2D",
+            opacity: "25%",
+            fontSize: "20px",
+            fontWeight: "700",
           }}
+          placeholder="Search for any word…"
         />
-      </InputRightElement>
-    </InputGroup>
+        <InputRightElement h="64px">
+          <Img src={searchIcon} alt="Search" onClick={handleSearchClick} />
+        </InputRightElement>
+      </InputGroup>
+      {inputError === true ? (
+        <Text color="#FF5252" fontWeight="400" fontSize="20px" marginTop="8px">
+          Whoops, can’t be empty…
+        </Text>
+      ) : null}
+    </Box>
   );
 }
